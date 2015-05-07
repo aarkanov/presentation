a131d755525ce0d66ac417a874fdd5c1937dd3b7
Alex Arkanov <alex@arkanov.com>
2015-05-07 16:02:26 -0400

    new presentation

---
+++ code/eval-buffer.el
@@ -0,0 +1,15 @@
+(defun jw-eval-buffer ()
+  "Evaluate the current buffer and display the result in a buffer."
+  (interactive)
+  (save-buffer)
+  (let* ((file-name (buffer-file-name (current-buffer)))
+         (file-extension (file-name-extension file-name))
+         (buffer-eval-command-pair (assoc file-extension jw-eval-buffer-commands)))
+    (if buffer-eval-command-pair
+        (let ((command (concat (cdr buffer-eval-command-pair) " " file-name)))
+          (shell-command-on-region (point-min) (point-max) command jw-eval-buffer-name nil)
+          (pop-to-buffer jw-eval-buffer-name)
+          (other-window 1)
+          (jw-eval-buffer-pretty-up-errors jw-eval-buffer-name)
+          (message ".."))
+      (message "Unknown buffer type"))))
